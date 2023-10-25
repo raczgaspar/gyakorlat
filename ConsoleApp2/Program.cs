005a0d78ae4294781e47770cd2f9c7cbe2656f3d
Boinx <racz.gaspar@stud.uni-obuda.hu>
2023-10-25 16:27:46 +0200

    loop to the project 2

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp2
             int a = int.Parse(Console.ReadLine());
             for (int i = 0; i < a; i++)
             {
-                Console.WriteLine(i+1);
+                
             }
         }
         
